c9e4bd849314dfbd8e218b01779ae9b072f4e491
Wallace Herlon <wherlon@hotmail.com>
2022-07-12 09:47:29 -0300

    Estruturas de repetição com EJS

--- index.js
+++ index.js
@@ -8,12 +8,23 @@ app.get('/:nome/:lang', (req, res) => {
     var nome = req.params.nome
     var lang = req.params.lang
     var exibirMsg = true
+
+    var produtos = [
+        {nome: 'Doritos', preco: 3.14},
+        {nome: 'Coca-Cola', preco: 5},
+        {nome: 'Leite', preco: 1.45},
+        {nome: 'Carne', preco: 15},
+        {nome: 'Redbull', preco: 6},
+        {nome: 'Nescau', preco: 4}
+    ]
+
     res.render('index', {
         nome: nome,
         lang: lang,
         empresa: 'Guia do programador',
         inscritos: 8040,
-        msg: exibirMsg
+        msg: exibirMsg,
+        produtos: produtos,
     })
 })
 
