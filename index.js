53cf49cb59a1cb484ea8bc8bd8f216c0e03a56de
Wallace Herlon <wherlon@hotmail.com>
2022-07-13 20:06:35 -0300

    Conexão com o banco de dados

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const connection = require('./database/database')
+
+//Database
+connection
+    .authenticate()
+    .then(() => {
+        console.log('Conexão feita com o banco de dados!')
+    })
+    .catch((msgErro => {
+        console.log(msgErro)
+    }))
 
 //Estou dizendo para o Express usar o EJS como view engine
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 //Body parser
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 // Rotas
