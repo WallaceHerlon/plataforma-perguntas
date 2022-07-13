f344ff8255cf7c28af428e3bb77b95291b63004e
Wallace Herlon <wherlon@hotmail.com>
2022-07-12 21:02:31 -0300

    Capturando dados de um formulário

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
 //Estou dizendo para o Express usar o EJS como view engine
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
+//Body parser
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+// Rotas
 app.get('/', (req, res) => {
     res.render('index')
 })
@@ -14,7 +20,9 @@ app.get('/perguntar', (req, res) => {
 })
 
 app.post('/salvarpergunta', (req, res) => {
-    res.send('Formulário recebido!')
+    var titulo = req.body.titulo
+    var descricao = req.body.descricao
+    res.send('Formulário recebido! titulo ' + titulo + ' ' + ' descricao ' + descricao)
 })
 
 app.listen(3000, () => {
